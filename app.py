1e22c147bb9132303d69ad52c262d7345563bfd9
Kallinteris Andreas <kallinteris@protonmail.com>
2022-10-24 09:54:58 +0300

    cleaned DDPG model

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import time
 #from multiagent_mujoco.mujoco_multi import MujocoMulti #https://github.com/schroederdewitt/multiagent_mujoco
 
 experience_replay = collections.namedtuple('Experience', 'old_state, actions, reward, new_state, terminal1')
-agent_spaces = collections.namedtuple('agent_def', 'observation_space, action_space')
 #note: actions, old_state, new_state are torch.Tensor reward is float32 scalar, terminal is bool
+agent_spaces = collections.namedtuple('agent_def', 'observation_space, action_space')
 
 TORCH_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -32,7 +32,7 @@ class experience_replay_buffer():
         return random.sample(self.buffer, min(batch_size, len(self.buffer)))
 
     def sample_batch_and_split(self, batch_size):
-        mini_batch = self.sample_batch(mini_batch_size) 
+        mini_batch = self.sample_batch(batch_size) 
         old_state_batch = torch.stack([exp.old_state for exp in mini_batch])
         new_state_batch = torch.stack([exp.new_state for exp in mini_batch])
         actions_batch = torch.stack([exp.actions for exp in mini_batch])
@@ -40,10 +40,7 @@ class experience_replay_buffer():
         return old_state_batch, actions_batch, reward_batch, new_state_batch
 
 
-#source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
-def soft_update(target, source, tau):
-    for target_param, param in zip(target.parameters(), source.parameters()):
-        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
 
 
 class centralized_ddpg_agent_actor(torch.nn.Module):
@@ -59,13 +56,6 @@ class centralized_ddpg_agent_actor(torch.nn.Module):
         output = torch.tanh(self.linear2(output))
         output = torch.tanh(self.linear3(output))
         return output
-
-def query_multi_agent_actor_team(actor_team, observations):
-    #result = []
-    #for actor in actor_team:
-        
-
-    None
 
 
 class centralized_ddpg_agent_critic(torch.nn.Module):
@@ -85,9 +75,66 @@ class centralized_ddpg_agent_critic(torch.nn.Module):
         return value
 
 
+#source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
+def soft_update_target_network(target, source, tau):
+    assert tau >= 0 and tau <= 1
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+
+class DDPG_model():
+    def __init__(self, num_actions, num_states):
+        assert num_actions > 0 and num_agents > 0
+
+        self.learning_rate = 0.999999 # gamma / discount_rate
+        self.learning_rate = 0.99 # gamma / discount_rate
+        #learning_rate = 1
+        self.target_rate = 0.01 # tau
+        self.mini_batch_size = 100
+        
+        self.actor = centralized_ddpg_agent_actor(num_actions, num_states) # mu
+        self.target_actor = copy.deepcopy(self.actor) # mu'
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters())#TODO check learning rate
+        self.critic = centralized_ddpg_agent_critic(num_actions, num_states) # q
+        self.target_critic = copy.deepcopy(self.critic) # q'
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+        self.critic_criterion = torch.nn.MSELoss()
+
+        self.erb = experience_replay_buffer(max_size = 1000 * 1000)
+
+    def query_actor(self, state):
+        return torch.clamp(self.actor(state) + torch.randn(num_actions).to(TORCH_DEVICE)*0.01, min = env.action_space.low[0], max = env.action_space.high[0])
+
+    def query_eval_actor(self, state):
+        return self.actor(state)
+
+    
+    def update_model_step(self):
+        #calulate input date for optimaizers from sampled mini-batch
+        old_state_batch, actions_batch, reward_batch, new_state_batch = self.erb.sample_batch_and_split(self.mini_batch_size)
+        q = self.critic(old_state_batch, actions_batch)
+        y = reward_batch + self.learning_rate * self.target_critic(new_state_batch, self.target_actor(new_state_batch))
+
+        #update critic
+        self.critic_optimizer.zero_grad()
+        critic_loss = self.critic_criterion(q, y)
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        #update actor
+        self.actor_optimizer.zero_grad()
+        policy_loss = -self.critic(old_state_batch, self.actor(old_state_batch)).mean()
+        policy_loss.backward()
+        self.actor_optimizer.step()
+
+        #update target networks
+        soft_update_target_network(self.target_actor, self.actor, self.target_rate)
+        soft_update_target_network(self.target_critic, self.critic, self.target_rate)
+
+
 if __name__ == "__main__":
-    #domain = "Swimmer"
-    domain = "Hopper"
+    domain = "Swimmer"
+    #domain = "Hopper"
     env = gymnasium.make(domain + '-v4')
     env_eval = gymnasium.make(domain + '-v4', reset_noise_scale = 0, render_mode='human')
     eval_file = open ('results/DDPG_' + domain + '_' + str(time.time()), 'w')
@@ -96,52 +143,20 @@ if __name__ == "__main__":
     num_actions = env.action_space.shape[0] #agent_size_modifier
     num_states = env.observation_space.shape[0] #len(env.observation_space(env.possible_agents[0]).shape) * agent_size_modifier
 
-    learning_rate = 0.999999 # gamma / discount_rate
-    learning_rate = 0.99 # gamma / discount_rate
-    #learning_rate = 1
-    target_rate = 0.01 # tau
-    mini_batch_size = 100
-
-    ddpg_agent_actor = centralized_ddpg_agent_actor(num_actions, num_states)
-    ddpg_agent_target_actor = copy.deepcopy(ddpg_agent_actor)
-    ddpg_agent_actor_optimizer = torch.optim.Adam(ddpg_agent_actor.parameters())#TODO check learning rate
-    ddpg_agent_critic = centralized_ddpg_agent_critic(num_actions, num_states)
-    ddpg_agent_target_critic = copy.deepcopy(ddpg_agent_critic)
-    ddpg_agent_critic_optimizer = torch.optim.Adam(ddpg_agent_critic.parameters())
-    critic_criterion = torch.nn.MSELoss()
+    DDPG = DDPG_model(num_actions, num_states)
 
 
-    erb = experience_replay_buffer(max_size = 1000 * 1000)
     for episode in range(10000):
         cur_state = torch.tensor(env.reset()[0], dtype=torch.float32).to(TORCH_DEVICE)
         for step in range(env.spec.max_episode_steps):
-            actions = torch.clamp(ddpg_agent_actor(cur_state) + torch.randn(num_actions).to(TORCH_DEVICE)*0.01, min = env.action_space.low[0], max = env.action_space.high[0])#to_CPU
+            actions = DDPG.query_actor(cur_state)
 
             new_state, reward, is_terminal, is_truncated, info = env.step(actions.tolist())
 
-            erb.add_experience(old_state = cur_state, actions= actions.detach(), reward = reward, new_state = torch.tensor(new_state, dtype=torch.float32).to(TORCH_DEVICE), is_terminal = is_terminal or is_truncated)
+            DDPG.erb.add_experience(old_state = cur_state, actions= actions.detach(), reward = reward, new_state = torch.tensor(new_state, dtype=torch.float32).to(TORCH_DEVICE), is_terminal = is_terminal or is_truncated)
             cur_state = torch.tensor(new_state, dtype=torch.float32).to(TORCH_DEVICE)
-
-            #calulate input date for optimaizers from sampled mini-batch
-            old_state_batch, actions_batch, reward_batch, new_state_batch = erb.sample_batch_and_split(mini_batch_size)
-            q = ddpg_agent_critic(old_state_batch, actions_batch)
-            y = reward_batch + learning_rate * ddpg_agent_target_critic(new_state_batch, ddpg_agent_target_actor(new_state_batch))
-
-            #update critic
-            ddpg_agent_critic_optimizer.zero_grad()
-            critic_loss = critic_criterion(q, y)
-            critic_loss.backward()
-            ddpg_agent_critic_optimizer.step()
-
-            #update actor
-            ddpg_agent_actor_optimizer.zero_grad()
-            policy_loss = -ddpg_agent_critic(old_state_batch, ddpg_agent_actor(old_state_batch)).mean()
-            policy_loss.backward()
-            ddpg_agent_actor_optimizer.step()
-
-            #update target networks
-            soft_update(ddpg_agent_target_actor, ddpg_agent_actor, target_rate)
-            soft_update(ddpg_agent_target_critic, ddpg_agent_critic, target_rate)
+            
+            DDPG.update_model_step()
             
             if is_truncated:
                 break
@@ -150,11 +165,10 @@ if __name__ == "__main__":
         cur_state = torch.tensor(env_eval.reset(seed=None)[0], dtype=torch.float32).to(TORCH_DEVICE)
         total_evalution_reward = 0
         for step in range(env_eval.spec.max_episode_steps):
-            actions = ddpg_agent_actor.forward(cur_state)
+            actions = DDPG.query_eval_actor(cur_state)
             new_state, reward, is_terminal, is_truncated, info = env_eval.step(actions.tolist())
             cur_state = torch.tensor(new_state, dtype=torch.float32).to(TORCH_DEVICE)
             total_evalution_reward += reward
-            #env.render()
             
             if is_truncated:
                 break
