50a1d167b67f2ab2f0cea8faf456aad372878335
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-02 17:00:06 +0200

    added twin critic

--- modules.py
+++ modules.py
@@ -33,6 +33,28 @@ class critic(torch.nn.Module):
         output = torch.tanh(self.linear3(output))
         return output
     
+class twin_critic(torch.nn.Module):
+    def __init__(self, action_space_size, observation_state_size, bias=False, device='cpu'):
+        super().__init__()
+        assert action_space_size > 0 and observation_state_size > 0
+        self.linear1_0 = torch.nn.linear(action_space_size + observation_state_size, 128, bias=bias, device=device)
+        self.linear2_0 = torch.nn.linear(128, 256, bias=bias, device=device)
+        self.linear3_0 = torch.nn.linear(256, 1, bias=bias, device=device)
+
+        self.linear1_1 = torch.nn.linear(action_space_size + observation_state_size, 128, bias=bias, device=device)
+        self.linear2_1 = torch.nn.linear(128, 256, bias=bias, device=device)
+        self.linear3_1 = torch.nn.linear(256, 1, bias=bias, device=device)
+
+    def forward(self, observations , actions):
+        assert isinstance(observations, torch.Tensor) and isinstance(actions, torch.Tensor)
+        output_0 = torch.tanh(self.linear1_0(torch.cat((observations, actions), dim = 1)))
+        output_0 = torch.tanh(self.linear2_0(output_0))
+        output_0 = torch.tanh(self.linear3_0(output_0))
+
+        output_1 = torch.tanh(self.linear1_1(torch.cat((observations, actions), dim = 1)))
+        output_1 = torch.tanh(self.linear2_1(output_1))
+        output_1 = torch.tanh(self.linear3_1(output_1))
+        return output_0, output_1
 
 
 #source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
