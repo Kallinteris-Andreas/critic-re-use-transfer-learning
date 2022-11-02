36564b13149dbb5bf6e986613ed25c3b6be275e3
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-02 07:48:17 +0200

    moved soft_update_target_network to modules.py

--- app.py
+++ app.py
@@ -15,14 +15,6 @@ from modules import *
 TORCH_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-#source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
-def soft_update_target_network(target, source, tau):
-    assert tau >= 0 and tau <= 1
-    assert isinstance(target, torch.nn.Module) and isinstance(source, torch.nn.Module)
-    for target_param, param in zip(target.parameters(), source.parameters()):
-        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
-
-
 class DDPG_model():
     def __init__(self, num_actions, num_states, yaml_config):
         assert num_actions > 0 and num_agents > 0
