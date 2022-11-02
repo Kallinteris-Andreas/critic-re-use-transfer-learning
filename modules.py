d2a44930b8a59fea5c28918c903b91b7856e024c
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-02 10:23:11 +0200

    typo fix

--- modules.py
+++ modules.py
@@ -36,8 +36,8 @@ class critic(torch.nn.Module):
 
 
 #source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
-    def soft_update_target_network(target, source, tau):
-        assert tau >= 0 and tau <= 1
-        assert isinstance(target, torch.nn.Module) and isinstance(source, torch.nn.Module)
-        for target_param, param in zip(target.parameters(), source.parameters()):
-            target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+def soft_update_target_network(target, source, tau):
+    assert tau >= 0 and tau <= 1
+    assert isinstance(target, torch.nn.Module) and isinstance(source, torch.nn.Module)
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
