141559ece9b3b22cf8562c0b2f04e3bf29ecef1a
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-03 11:39:01 +0200

    added TD3 algorithm

--- app.py
+++ app.py
@@ -70,6 +70,83 @@ class DDPG_model():
         soft_update_target_network(self.target_critic, self.critic, self.target_update_rate)
 
 
+class TD3_model():
+    #TODO handle truncated cases
+    def __init__(self, num_actions, num_states, yaml_config):
+        assert num_actions > 0 and num_agents > 0
+        self.num_actions = num_actions
+        self.total_step_iterations = 0
+
+        self.discount_rate = yaml_config['TD3']['gamma']
+        self.target_update_rate = yaml_config['TD3']['tau']
+        self.mini_batch_size = yaml_config['TD3']['N']
+        self.noise_exploration_standard_deviation = yaml_config['TD3']['sigma_explore']
+        self.noise_policy_standard_deviation = yaml_config['TD3']['sigma_policy']
+        self.noise_policy_clip = yaml_config['TD3']['noise_policy_clip']
+        self.policy_update_frequency = yaml_config['TD3']['d']
+        
+        self.actor = actor(num_actions, num_states, bias=True, device=TORCH_DEVICE) # mu
+        self.target_actor = copy.deepcopy(self.actor) # mu'
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), yaml_config['TD3']['optimizer_gamma'])
+        self.critics= twin_critic(num_actions, num_states, bias=False, device=TORCH_DEVICE) # q0-1
+        self.target_critics = copy.deepcopy(self.critics) # q0-1'
+        self.critics_optimizer = torch.optim.Adam(self.critics.parameters(), yaml_config['TD3']['optimizer_gamma'])
+
+
+        #self.critic0 = critic(num_actions, num_states, bias=False, device=TORCH_DEVICE) # q0
+        #self.critic1 = critic(num_actions, num_states, bias=False, device=TORCH_DEVICE) # q1
+        #self.target_critic0 = copy.deepcopy(self.critic0) # q0'
+        #self.target_critic1 = copy.deepcopy(self.critic1) # q1
+        #breakpoint()
+        #self.critic_optimizer = torch.optim.Adam(self.critic0.parameters(), yaml_config['TD3']['optimizer_gamma'])
+
+        self.erb = experience_replay_buffer(yaml_config['TD3']['experience_replay_buffer_size'])
+
+    def query_actor(self, state, add_noise=True):
+        if add_noise:
+            return torch.clamp(self.actor(state) + torch.randn(self.num_actions, device=TORCH_DEVICE)*self.noise_exploration_standard_deviation, min = env.action_space.low[0], max = env.action_space.high[0])
+        else:
+            return self.actor(state)
+
+
+    def train_model_step(self):
+        if len(self.erb.buffer) < self.mini_batch_size:
+            return
+
+        old_state_batch, actions_batch, reward_batch, new_state_batch = self.erb.sample_batch_and_split(self.mini_batch_size)
+
+
+        #update critic
+        #y = reward_batch + self.discount_rate * torch.min(self.target_critic0(new_state_batch, target_actions), self.target_critic1(new_state_batch, target_actions))
+        target_policy_noise = (torch.randn(self.mini_batch_size, 1, device=TORCH_DEVICE)*self.noise_policy_standard_deviation).clamp(min= -self.noise_policy_clip, max=self.noise_policy_clip)
+        target_actions_batch = torch.clamp(self.target_actor(new_state_batch) + target_policy_noise, min = env.action_space.low[0], max = env.action_space.high[0])
+        qt0, qt1 = self.target_critics(new_state_batch, target_actions_batch)
+        y = reward_batch + self.discount_rate * torch.min(qt0, qt1)
+        q0, q1 = self.critics(old_state_batch, actions_batch)
+
+        #print('q: ' + str(torch.transpose(q, 0, 1).detach().to('cpu')))
+        #print('y: ' + str(torch.transpose(y, 0, 1).detach().to('cpu')))
+        critics_loss = torch.nn.functional.mse_loss(q0, y) + torch.nn.functional.mse_loss(q1, y)
+        #critic_loss = torch.nn.functional.mse_loss(q, y)
+        self.critics_optimizer.zero_grad()
+        critics_loss.backward()
+        #print('Critic Loss: ' + str(critics_loss.detach().to('cpu')))
+        self.critics_optimizer.step()
+
+        if (++self.total_step_iterations % self.policy_update_frequency) == 0:
+            #update actor
+            policy_loss = (-self.critics(old_state_batch, self.actor(old_state_batch))[0]).mean()
+
+            self.actor_optimizer.zero_grad()
+            policy_loss.backward()
+            #print('Policy Loss: ' + str(policy_loss.detach().to('cpu')))
+            self.actor_optimizer.step()
+
+            #update target networks
+            soft_update_target_network(self.target_actor, self.actor, self.target_update_rate)
+            soft_update_target_network(self.target_critics, self.critics, self.target_update_rate)
+
+
 if __name__ == "__main__":
     config = yaml.safe_load(open('config.yaml', 'r'))
 
@@ -83,6 +160,8 @@ if __name__ == "__main__":
     match config['domain']['algo']:
         case 'DDPG':
             model = DDPG_model(num_actions, num_states, config)
+        case 'TD3':
+            model = TD3_model(num_actions, num_states, config)
         case _:
             print('invalid learning algorithm')
             exit(1)
