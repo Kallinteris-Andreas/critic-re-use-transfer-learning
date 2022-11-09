b88479bc25434808d0786addc923514685180f9d
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-09 11:08:49 +0200

    +`eval_policy` which allows to re_eval the actor

---
+++ eval_policy.py
@@ -0,0 +1,34 @@
+import torch
+import gymnasium
+import argparse
+import yaml
+from modules import *
+
+TORCH_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--res")
+    args = parser.parse_args()
+
+    config = yaml.safe_load(open(args.res + 'config.yaml', 'r'))
+    env_eval = gymnasium.make(config['domain']['name'])
+    
+    mu = actor(env_eval.action_space.shape[0], env_eval.observation_space.shape[0], env_eval.action_space.high[0], config['TD3']['mu_bias'], device=TORCH_DEVICE)
+    mu.load_state_dict(torch.load(args.res + '/best_actor.pt'))
+
+    #eval
+    for episode in range(25):
+        cur_state = torch.tensor(env_eval.reset()[0], dtype=torch.float32, device=TORCH_DEVICE)
+        total_evalution_return = 0
+        for step in range(env_eval.spec.max_episode_steps):
+            actions = mu(cur_state)
+            new_state, reward, is_terminal, is_truncated, info = env_eval.step(actions.tolist())
+
+            cur_state = torch.tensor(new_state, dtype=torch.float32, device=TORCH_DEVICE)
+
+            total_evalution_return += reward
+            
+            if is_terminal:
+                break
+        print("Episode: " + str(episode) + ' Return: ' + str(total_evalution_return))
