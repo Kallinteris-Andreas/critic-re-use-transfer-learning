0f9dd9a85741c5348a11dedb98dccf14b492315b
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-09 09:22:08 +0200

    added CLI args & now saves the best actor

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import yaml
 import os
 import shutil
 import sys
+import math
+import argparse
 from ERB import *
 from modules import *
 from DDPG import *
@@ -17,10 +19,15 @@ TORCH_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 if __name__ == "__main__":
-    config = yaml.safe_load(open('config.yaml', 'r'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default='config.yaml')
+    args = parser.parse_args()
+    config = yaml.safe_load(open(args.res, 'r'))
 
+    #env = gymnasium.make(config['domain']['name'])
+    #env_eval = gymnasium.make(config['domain']['name'], reset_noise_scale = 0, render_mode='human')
     env = gymnasium.make(config['domain']['name'])
-    env_eval = gymnasium.make(config['domain']['name'], reset_noise_scale = 0, render_mode='human')
+    env_eval = gymnasium.make(config['domain']['name'], render_mode='human')
 
     num_agents = 1
     num_actions = env.action_space.shape[0] #agent_size_modifier
@@ -38,16 +45,17 @@ if __name__ == "__main__":
     eval_path = 'results/' + config['domain']['algo'] + '_' + config['domain']['name'] + '_' + str(time.time()) 
     os.makedirs(eval_path)
     eval_file = open(eval_path + '/score.csv', 'w+')
-    shutil.copyfile('./config.yaml', eval_path + '/config.yaml')
+    shutil.copyfile(args.res, eval_path + '/config.yaml')
+    eval_max_return = -math.inf
 
 
     for episode in range(config['domain']['episodes']):
         cur_state = torch.tensor(env.reset()[0], dtype=torch.float32, device=TORCH_DEVICE)
         for step in range(env.spec.max_episode_steps):
             actions = model.query_actor(cur_state)
-            #print('Actions: ' + str(actions) + ' State: ' + str(cur_state))
 
             new_state, reward, is_terminal, is_truncated, info = env.step(actions.tolist())
+            #print('step: ' + str(step) + ' state: ' + str(cur_state.tolist()) + ' actions: ' + str(actions.tolist()) + ' reward: ' + str(reward))#this is a debug line
 
             model.erb.add_experience(old_state = cur_state, actions = actions.detach(), reward = reward, new_state = torch.tensor(new_state, dtype=torch.float32, device=TORCH_DEVICE), is_terminal = is_terminal)
             cur_state = torch.tensor(new_state, dtype=torch.float32, device=TORCH_DEVICE)
@@ -60,17 +68,22 @@ if __name__ == "__main__":
 
         #evaluate episode
         cur_state = torch.tensor(env_eval.reset(seed=None)[0], dtype=torch.float32, device=TORCH_DEVICE)
-        total_evalution_reward = 0
+        total_evalution_return = 0
         for step in range(env_eval.spec.max_episode_steps):
             actions = model.query_actor(cur_state, add_noise=False)
             new_state, reward, is_terminal, is_truncated, info = env_eval.step(actions.tolist())
             #if episode > 100:
-                #print('step: ' + str(step) + 'state: ' + str(cur_state.tolist()) + ' actions: ' + str(actions.tolist()) + ' reward: ' + str(reward))#this is a debug line
+                #print('step: ' + str(step) + ' state: ' + str(cur_state.tolist()) + ' actions: ' + str(actions.tolist()) + ' reward: ' + str(reward))#this is a debug line
             cur_state = torch.tensor(new_state, dtype=torch.float32, device=TORCH_DEVICE)
-            total_evalution_reward += reward
+            total_evalution_return += reward
             
             if is_terminal:
                 break
-        print("Episode: " + str(episode) + ' reward: ' + str(total_evalution_reward))
-        eval_file.write(str(total_evalution_reward) + '\n')
+        print("Episode: " + str(episode) + ' return: ' + str(total_evalution_return))
+        eval_file.write(str(total_evalution_return) + '\n')
+        if (eval_max_return < total_evalution_return):
+            eval_max_return = total_evalution_return
+            best_actor = copy.deepcopy(model.actor)
+    print('max return: ' + str(eval_max_return))
     print('Finished score can be found at: ' + eval_path + '/score.csv')
+    torch.save(best_actor.state_dict(), eval_path + '/best_actor.pt')
