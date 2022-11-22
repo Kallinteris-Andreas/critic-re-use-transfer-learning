2b9e6489fd30251431a732608443d5e53870759f
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-22 17:49:49 +0200

    cleanup

--- modules.py
+++ modules.py
@@ -28,7 +28,7 @@ class critic(torch.nn.Module):
         self.linear2 = torch.nn.Linear(256, 256, bias=bias, device=device)
         self.linear3 = torch.nn.Linear(256, 1, bias=bias, device=device)
 
-    def forward(self, observations , actions):
+    def forward(self, observations, actions):
         assert isinstance(observations, torch.Tensor) and isinstance(actions, torch.Tensor)
         output = torch.relu(self.linear1(torch.cat((observations, actions), dim = 1)))
         output = torch.relu(self.linear2(output))
@@ -59,7 +59,7 @@ class twin_critic(torch.nn.Module):
         return output_0, output_1
 
 
-#source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
+# source: https://github.com/ghliu/pytorch-ddpg/blob/master/util.py
 def soft_update_target_network(target, source, tau):
     assert tau >= 0 and tau <= 1
     assert isinstance(target, torch.nn.Module) and isinstance(source, torch.nn.Module)
