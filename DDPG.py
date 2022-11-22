11adf02ee5d5a011b6bcd91a3fade91be1721677
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-22 15:56:02 +0200

    `DDPG` cleanup

--- DDPG.py
+++ DDPG.py
@@ -15,7 +15,7 @@ class DDPG_model():
         self.target_update_rate = yaml_config['DDPG']['tau']
         self.mini_batch_size = yaml_config['DDPG']['N']
         self.noise_standard_deviation = yaml_config['DDPG']['sigma']
-        
+
         self.actor = actor(num_actions, num_states, max_action, yaml_config['TD3']['mu_bias'], device=TORCH_DEVICE) # mu
         self.target_actor = copy.deepcopy(self.actor) # mu'
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), yaml_config['DDPG']['optimizer_gamma'])
@@ -38,24 +38,20 @@ class DDPG_model():
 
         old_state_batch, actions_batch, reward_batch, new_state_batch, terminal_batch = self.erb.sample_batch_and_split(self.mini_batch_size)
 
-        #update critic
+        # update critic
         q = self.critic(old_state_batch, actions_batch)
         y = reward_batch + ~terminal_batch * self.discount_rate * self.target_critic(new_state_batch, self.target_actor(new_state_batch))
-        #print('q: ' + str(torch.transpose(q, 0, 1).detach().to('cpu')))
-        #print('y: ' + str(torch.transpose(y, 0, 1).detach().to('cpu')))
-        self.critic_optimizer.zero_grad()
         critic_loss = torch.nn.functional.mse_loss(q, y)
-        #print('Critic Lost: ' + str(critic_loss.detach().to('cpu')))
+        self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        #update actor
-        self.actor_optimizer.zero_grad()
+        # update actor
         policy_loss = (-self.critic(old_state_batch, self.actor(old_state_batch))).mean()
-        #print('Policy Loss: ' + str(policy_loss.detach().to('cpu')))
+        self.actor_optimizer.zero_grad()
         policy_loss.backward()
         self.actor_optimizer.step()
 
-        #update target networks
+        # update target networks
         soft_update_target_network(self.target_actor, self.actor, self.target_update_rate)
         soft_update_target_network(self.target_critic, self.critic, self.target_update_rate)
