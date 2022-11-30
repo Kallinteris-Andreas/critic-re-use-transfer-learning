c6a56f269a69c02859d722f4da3bf10f46407adf
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-30 11:02:45 +0200

    `ERB` added `sample` for multi agent algorithms

--- ERB.py
+++ ERB.py
@@ -30,3 +30,43 @@ class experience_replay_buffer():
         reward_batch = torch.tensor([[exp.reward for exp in mini_batch]], dtype=torch.float32, device=TORCH_DEVICE).transpose(0, 1)
         terminal_batch = torch.tensor([[exp.is_terminal for exp in mini_batch]], device=TORCH_DEVICE).transpose(0, 1)
         return old_state_batch, actions_batch, reward_batch, new_state_batch, terminal_batch
+
+    def sample_batch_and_split_ma(self, batch_size: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        mini_batch = self.sample_batch(batch_size)
+        old_state_global_batch = torch.stack([exp.old_state['global'] for exp in mini_batch])
+        # convert old_state_locals_batch from a list of list to a list of torch.tensor
+        old_state_locals_batch = [[] for agent_id in range(len(mini_batch[0].old_state['local']))]
+        for exp in mini_batch:
+            for index, l in enumerate(exp.old_state['local']):
+                old_state_locals_batch[index].append(l)
+        for agent_id in range(len(old_state_locals_batch)):
+            old_state_locals_batch[agent_id] = torch.stack(old_state_locals_batch[agent_id])
+
+        # convert new_state_locals_batch from a list of list to a list of torch.tensor
+        new_state_global_batch = torch.stack([exp.new_state['global'] for exp in mini_batch])
+        new_state_locals_batch = [[] for agent_id in range(len(mini_batch[0].new_state['local']))]
+        for exp in mini_batch:
+            for index, l in enumerate(exp.new_state['local']):
+                new_state_locals_batch[index].append(l)
+        for agent_id in range(len(new_state_locals_batch)):
+            new_state_locals_batch[agent_id] = torch.stack(new_state_locals_batch[agent_id])
+
+        actions_global_batch = torch.stack([exp.actions['global'] for exp in mini_batch])
+        actions_locals_batch = [[] for agent_id in range(len(mini_batch[0].actions['local']))]
+        for exp in mini_batch:
+            for index, l in enumerate(exp.actions['local']):
+                actions_locals_batch[index].append(l)
+        for agent_id in range(len(actions_locals_batch)):
+            actions_locals_batch[agent_id] = torch.stack(actions_locals_batch[agent_id])
+
+        rewards_batch = [[] for agent_id in range(len(mini_batch[0].reward))]
+        for exp in mini_batch:
+            for index, l in enumerate(exp.reward):
+                rewards_batch[index].append(l)
+
+        terminals_batch = [[] for agent_id in range(len(mini_batch[0].is_terminal))]
+        for exp in mini_batch:
+            for index, l in enumerate(exp.is_terminal):
+                terminals_batch[index].append(l)
+
+        return old_state_global_batch, old_state_locals_batch, actions_global_batch, actions_locals_batch, rewards_batch, new_state_global_batch, new_state_locals_batch, terminals_batch
