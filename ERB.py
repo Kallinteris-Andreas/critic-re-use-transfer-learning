e6340ad1e53d33ccf6215fdd6ba3801eefe2ddbe
Kallinteris Andreas <kallinteris@protonmail.com>
2023-11-07 12:22:27 +0200

    update ERB to support `CUDA`

--- ERB.py
+++ ERB.py
@@ -15,7 +15,7 @@ class experience_replay_buffer():
         self.torch_device = device
 
     def add_experience(self, old_state: torch.Tensor, actions: torch.Tensor, reward: float, new_state: torch.Tensor, is_terminal: bool) -> None:
-        self.buffer.append(experience_replay(old_state, actions, reward, new_state, is_terminal))
+        self.buffer.append(experience_replay(old_state.clone().to("cpu"), actions.clone().to("cpu"), reward, new_state.clone().to("cpu"), is_terminal))
 
     def sample_batch(self, batch_size: int) -> list[experience_replay]:
         assert len(self.buffer) != 0
@@ -23,9 +23,9 @@ class experience_replay_buffer():
 
     def sample_batch_and_split(self, batch_size: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         mini_batch = self.sample_batch(batch_size)
-        old_state_batch = torch.stack([exp.old_state for exp in mini_batch])
-        new_state_batch = torch.stack([exp.new_state for exp in mini_batch])
-        actions_batch = torch.stack([exp.actions for exp in mini_batch])
+        old_state_batch = torch.stack([exp.old_state for exp in mini_batch]).to(self.torch_device)
+        new_state_batch = torch.stack([exp.new_state for exp in mini_batch]).to(self.torch_device)
+        actions_batch = torch.stack([exp.actions for exp in mini_batch]).to(self.torch_device)
         reward_batch = torch.tensor([[exp.reward for exp in mini_batch]], dtype=torch.float32, device=self.torch_device).transpose(0, 1)
         terminal_batch = torch.tensor([[exp.is_terminal for exp in mini_batch]], device=self.torch_device).transpose(0, 1)
         return old_state_batch, actions_batch, reward_batch, new_state_batch, terminal_batch
